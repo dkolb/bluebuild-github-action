4b3cd084974407d5dc3f533f882a1a3d61832b09
David Kolb <git@dkub.dev>
2024-11-09 14:53:18 -0500

    feat: allow providing options to `bluebuild build` command

---
+++ build_opts_check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Expected env vars:
+# SQUASH_INPUT_VALUER
+# BUILD_OPTS
+
+if [ "$SQUASH_INPUT_VALUE" != "true" ]; then
+  echo "Not using squash. No need to check build opts."
+  exit 0
+fi
+
+if grep -qE '(-B)|(--build-driver)' <<< "$BUILD_OPTS"; then
+  echo 'Cannot provide --build-driver in build_opts while squash = true'
+  exit 1
+fi
+
+if grep -qE '(-s)|(--squash)' <<< "$BUILD_OPTS"; then
+  echo 'Cannot provide --squash in build_opts while squash = true'
+  exit 1
+fi
+
+if grep -qE '(-p)|(--push)' <<< "$BUILD_OPTS"; then
+  echo 'Please do not provide --push in build_opts as the action provides it' \
+    ' by default anyway.'
+  exit 1
+fi
+
+exit 0
